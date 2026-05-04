1cb44350991a7ea6f938ecb25b7afe925c0a30b4
Priyanka Hichkad <priyankahichkad@gmail.com>
2026-05-04 10:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,6 @@ from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 import streamlit as st
 
-from model_utils import HybridPPVModel
-
 warnings.filterwarnings('ignore')
 np.random.seed(42)
 
